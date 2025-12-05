6d56c30089f5a189a338dd3d3095ba4a9ea43195
Thibaud Arguillere <thibaud.arguillere@hyland.com>
2025-12-05 09:45:44 +0100

    Remove warning

--- nuxeo-labs-pdf-toolkit-core/src/main/java/nuxeo/labs/pdf/toolkit/PDFToImages.java
+++ nuxeo-labs-pdf-toolkit-core/src/main/java/nuxeo/labs/pdf/toolkit/PDFToImages.java
@@ -27,7 +27,6 @@ import java.io.Serializable;
 import java.util.Base64;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.imageio.ImageIO;
